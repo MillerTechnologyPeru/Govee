8710d241853dd779b12d67268e968a5141aad733
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-16 01:23:19 -0700

    Add `GloveeModel`

---
+++ Sources/Govee/Model.swift
@@ -0,0 +1,27 @@
+//
+//  Model.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 3/16/23.
+//
+
+import Foundation
+
+/// Glovee Model
+public enum GloveeModel: String, Codable, CaseIterable {
+    
+    case h5072 = "H5072"
+    case h5074 = "H5074"
+    case h5075 = "H5075"
+    case h5101 = "H5101"
+    case h5102 = "H5102"
+    case h5177 = "H5177"
+    case h5178 = "H5178"
+    case h5179 = "H5179"
+    case h5182 = "H5182"
+    case h5183 = "H5183"
+    case h5184 = "H5184"
+    case h5185 = "H5185"
+    case h5198 = "H5198"
+}
+
