5fe3190142a0da3297bb3d800f95018eb2d86efb
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-25 12:43:46 -0700

    Add `GoveeAdvertisement.id`

--- Sources/Govee/Govee.swift
+++ Sources/Govee/Govee.swift
@@ -1,7 +1,29 @@
 import Foundation
 import Bluetooth
+import GATT
 
 public enum GoveeAdvertisement: Equatable, Hashable {
     
     case thermometer(Thermometer)
+}
+
+extension GoveeAdvertisement: Identifiable {
+    
+    public var id: BluetoothAddress {
+        switch self {
+        case .thermometer(let thermometer):
+            return thermometer.name.address
+        }
+    }
+}
+
+public extension GoveeAdvertisement {
+    
+    init?<T: GATT.AdvertisementData>(_ advertisementData: T) {
+        if let thermometer = Thermometer(advertisementData) {
+            self = .thermometer(thermometer)
+        } else {
+            return nil
+        }
+    }
 }
